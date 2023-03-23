078c17b189146fd464ca42e1ff887a3657882273
Donatas Noreika <donoras@gmail.com>
2023-03-23 08:29:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from modules.python_kursas import Kursas, PythonKursas
+
+kursas1 = Kursas("Muzika", "Jotautas", 100)
+kursas2 = PythonKursas("Python pagrindai", "Donatas", 80)
+
+kursas1.destyti()
+kursas2.destyti()
